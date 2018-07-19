257668703cd0261598bcf855434d0d8aa9b76a80
Chang Sophia <you@example.com>
2018-07-19 14:30:21 +0800

    1st

--- Finalcountdown/ViewController.swift
+++ Finalcountdown/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
+var timer: Timer?
+var goal: Date!
+var now: Date!
+
 class ViewController: UIViewController {
-
+    
+  
+    @IBOutlet weak var goalTextField: UITextField!
+    @IBOutlet weak var nowTextField: UITextField!
+    @IBOutlet weak var myDatePickerValue: UIDatePicker!
+    @IBOutlet weak var intervalTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myDatePickerValue.minimumDate = Date()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func didReceiveMemoryWarning(){
+    super.didReceiveMemoryWarning()
     }
-
-
+    override func viewDidDisappear(_ animated: Bool) {
+        if timer != nil {
+            timer?.invalidate()
+        }
+    }
+    
+    @IBAction func myDatePickerValue(_ sender: UIDatePicker) {
+        goal = sender.date
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        let time = formatter.string(from: now)
+        self.nowTextField.text = time
+        let interval = goal.timeIntervalSince(now)
+        goalTextField.text = formatter.string(from: myDatePickerValue.date)
+        let integerinterval = Int(interval/60/60/24+1)
+        intervalTextField.text = ("\(integerinterval)")
+        print(integerinterval)
+    }
 }
-
